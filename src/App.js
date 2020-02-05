48cb28d1a85d28be783ccb46eddcaacdb48d7379
Harika Tirunagari <36506029+Tirunagari-Harika@users.noreply.github.com>
2020-02-06 01:32:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+/* import Main from "./components/mountRoutes/main"; */
+/* import Main from "./containers/main"; */
+
+import MainRouter from "./new-trial/mainRouter";
+
+function App() {
+  return (
+    <div className="App">
+      <MainRouter />
+    </div>
+  );
+}
+
+export default App;
